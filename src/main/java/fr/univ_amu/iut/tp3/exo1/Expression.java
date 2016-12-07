3a29b93df8509670914d4c3732e763d808919dc4
GeeksdelaCave <geeksdelacave@laposte.net>
2016-12-07 15:59:54 +0100

    UTD - Doc for Expression

--- src/main/java/fr/univ_amu/iut/tp3/exo1/Expression.java
+++ src/main/java/fr/univ_amu/iut/tp3/exo1/Expression.java
@@ -1,5 +1,9 @@
 package fr.univ_amu.iut.tp3.exo1;
 
 public abstract class Expression {
+    /**
+     * Méthode pour renvoyer la valeur de l'expression
+     * @return La valeur de l'expression implémentée
+     */
     public abstract int calculer();
 }
